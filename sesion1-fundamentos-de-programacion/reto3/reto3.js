0cb321b557246a8897d6546c90f38025a9d4ad83
JoseLAmador <josel.amador.s@gmail.com>
2019-11-22 00:24:31 -0600

    Solución reto 3

---
+++ sesion1-fundamentos-de-programacion/reto3/reto3.js
@@ -0,0 +1,9 @@
+// Reto 3, Mostrar si es número es PAR o IMPAR, practicando ciclos
+
+for(var number = 0; number <= 100; number++) {
+  if ( number % 2 === 0) {
+    console.log(`El número ${number} es par`)
+  } else {
+    console.log(`El número ${number} es impar`)
+  }
+}
