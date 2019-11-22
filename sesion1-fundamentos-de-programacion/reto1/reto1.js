f13a6e39ba6f61da93338db08d6e718ba49c7820
JoseLAmador <josel.amador.s@gmail.com>
2019-11-22 00:24:13 -0600

    Solución reto 1

---
+++ sesion1-fundamentos-de-programacion/reto1/reto1.js
@@ -0,0 +1,36 @@
+// RETO 1, Crear Variables
+
+// String
+
+var firstName = "Jose";
+console.log(firstName);
+
+// Number
+
+var age = 26;
+console.log(age);
+
+// Boolean
+var playSoccer = true;
+console.log(playSoccer);
+// Undefined
+var lastName;
+console.log(lastName);
+
+// Null
+var score = null;
+console.log(score);
+
+// Object
+var product = {
+  title: "Xbox One",
+  price: 4500,
+  description: "La única consola diseñada para jugar los mejores juegos del pasado, del presente y del futuro.",
+  offer: true
+};
+console.log(product);
+
+// Array
+
+var favoriteSeries = ["Black Mirror", "Stranger Things", "The Good Doctor"];
+console.log(favoriteSeries);
